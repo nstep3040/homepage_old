43179a4f81ef094f341304b0e5451c86af852212
nstep3040 <ns@nstep.co.kr>
2022-07-19 09:50:13 +0900

    20220719_repair

---
+++ js/lnb0.js
@@ -0,0 +1,10 @@
+document.write(`
+    <div class="lnb0">
+    <h2>&nbsp;&nbsp;&nbsp;고객센터</h2>
+    <p>&nbsp;&nbsp;E-mail : <a href="mailto:ns@nstep.co.kr">ns@nstep.co.kr</a><br />
+    &nbsp;&nbsp;Tel : 053)604-3040<br />
+    &nbsp;&nbsp;Fax : 053)604-3042<br />
+    &nbsp;&nbsp;주소 : 대구광역시 북구 유통단지로 16<br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;산업용재관 23동 4호</p>
+    </div>
+`);
--- js/rnb.js
+++ js/rnb.js
@@ -1,27 +1,25 @@
 document.write(`
-        <div class="rnb ">
-            <!DOCTYPE html>
-            <html lang="ko">
+    <div class="rnb ">
+        <!DOCTYPE html>
+        <html lang="ko">
 
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <link rel="stylesheet" href="./css/rnb.css">
-            </head>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="./css/rnb.css">
+        </head>
 
-            <body>
-                <div class="rnb">
-                    <a href="/html/sub_6.html"><img src="../img/aeg.png"></a>
-                    <a href="/html/sub_3.html"><img src="../img/go.png"></a>
-                    <a href="/html/sub_5.html"><img src="../img/sa.png"></a>
-                    <a href="/html/sub_11.html"><img src="../img/te.png"></a>
-                    <a href="https://blog.naver.com/nstep3040" target="_blank"><img src="../img/blog_link.png?ver=1"></a>
-                </div>
-            </body>
+        <body>
+            <div class="rnb">
+                <a href="/html/sub_6.html"><img src="../img/aeg.png"></a>
+                <a href="/html/sub_3.html"><img src="../img/go.png"></a>
+                <a href="/html/sub_5.html"><img src="../img/sa.png"></a>
+                <a href="/html/sub_11.html"><img src="../img/te.png"></a>
+                <a href="https://blog.naver.com/nstep3040" target="_blank"><img src="../img/blog_link.png?ver=1"></a>
+            </div>
+        </body>
 
-            </html>
-        </div>
-    </div>
+        </html>
     </div>
 `);
