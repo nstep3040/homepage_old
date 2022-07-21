a9de8f8464481bcf8d719c87c38ea9db1f452d99
nstep3040 <ns@nstep.co.kr>
2022-07-21 16:29:34 +0900

    20220721_final

--- js/mainRnb.js
+++ js/mainRnb.js
@@ -5,7 +5,7 @@ document.write(`
                 <a href="./html/sub_3.html"><img src="./img/go.png"></a>
                 <a href="./html/sub_5.html"><img src="./img/sa.png"></a>
                 <a href="./html/sub_11.html"><img src="./img/te.png"></a>
-                <a href="https://blog.naver.com/nstep3040" target="_blank"><img src="../img/blog_link.png?ver=1"></a>
+                <a href="https://blog.naver.com/nstep3040" target="_blank"><img src="./img/blog_link.png?ver=1"></a>
             </div>
         </body>
 `);
