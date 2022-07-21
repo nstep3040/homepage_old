b80b7dc539a190d8c59876d04fa9cb518f77b419
nstep3040 <ns@nstep.co.kr>
2022-07-21 16:14:09 +0900

    20220721_repair09

--- js/mainHead.js
+++ js/mainHead.js
@@ -3,10 +3,10 @@ document.write(`
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="naver-site-verification" content="40eb32b689e3f6b1c4116ba649c3d1414064d56e" />
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/sub1.css">
-	<link rel="stylesheet" href="../include/css/rnb.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/sub1.css">
+	<link rel="stylesheet" href="./include/css/rnb.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/notosanskr.css">
 	<style>
 		* { font-family:'Noto Sans KR','나눔 고딕','Nanum Gothic','맑은 고딕' }
@@ -20,10 +20,10 @@ document.write(`
             <img src="./img/header.jpg">
             <div class="inner">
                 <p><a href="./index.html">HOME</p></a>
-                <p><a href="./sub_1.html">INDUSTRY</p></a>
-                <p><a href="./sub_17.html">SOFTWARE</p></a>
-                <p><a href="./sub_14.html">DOWNLOADS</p></a>
-                <p><a href="./sub_15.html">CONTACT US</p></a>
+                <p><a href="./html/sub_1.html">INDUSTRY</p></a>
+                <p><a href="./html/sub_17.html">SOFTWARE</p></a>
+                <p><a href="./html/sub_14.html">DOWNLOADS</p></a>
+                <p><a href="./html/sub_15.html">CONTACT US</p></a>
             </div>
         </div>
         </head>
